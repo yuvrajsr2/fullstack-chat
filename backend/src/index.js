900955a97b082e01974794a8f1ff838a9fa39e3d
Yuvraj Singh Rajput <75319329+yuvrajsr2@users.noreply.github.com>
2025-10-20 23:44:31 -0400

    Fix #2 for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -36,7 +36,7 @@ const PORT = process.env.PORT;
 if (process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
-    app.get("/*", (req, res) =>{
+    app.get(/^\/(?!api).*/, (req, res) =>{
         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
     })
 }
